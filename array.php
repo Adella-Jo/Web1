d631b5c79a632b99db800e7379e7b7e221bfc6a7
Adella-Jo <70190535+Adella-Jo@users.noreply.github.com>
2020-08-29 21:56:26 +0900

    Web1
    반복문. php 활용해서 data 폴더 안에 파일을 만들면 따로 입력하지 않아도 자동으로 목차가 생겨서 그 링크로 이동할 수 있게 하는...  ㅋㅋㅋㅋㅋ 아아아앙ㄱ

---
+++ array.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+  <title>Array</title>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>Array></h1>
+      <?php
+
+
+      ?>
+
+  </body>
+</html>
---
+++ conditional.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+  <title></title>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>Conditional</h1>
+    <h2>If</h2>
+    <?php
+      echo '1<br>';
+
+      if(false) {
+        echo '2<br>';
+      } else {
+        echo '3-3<br>';
+
+      }
+
+
+
+
+      ?>
+  </body>
+</html>
