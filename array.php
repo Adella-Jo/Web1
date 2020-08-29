fc846278038d27dada31bdad567cd28b5e90c1cf
Adella-Jo <70190535+Adella-Jo@users.noreply.github.com>
2020-08-29 22:21:48 +0900

    Web1
    배열을 활용한 정리.

--- array.php
+++ array.php
@@ -7,8 +7,14 @@
   <body>
     <h1>Array></h1>
       <?php
-
-
+        $myname = array('조', '은', '영', '짱');
+        echo $myname[2].'<br>';
+        echo $myname[0].'<br>';
+        echo count ($myname).'<br>';
+        var_dump(count($myname));
+        array_push($myname, '최고');
+        var_dump($myname);
+        echo '바보';
       ?>
 
   </body>
