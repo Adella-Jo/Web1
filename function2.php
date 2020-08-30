470ffeb0d2ad7a109cb63a216c0ff14c6e63b73e
Adella-Jo <70190535+Adella-Jo@users.noreply.github.com>
2020-08-30 20:40:35 +0900

    Web1
    php를 이용해서 좀 더 간단하게 리로드할 수 있는 방ㅂㅓ업...

---
+++ function2.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>functions</title>
+  </head>
+
+  <body>
+    <h1>Function</h1>
+    <h2>Basic</h2>
+    <?php
+      function basic () {
+        print("Lorem ipsum dolor sit amet 1<br>");
+        print("Lorem ipsum dolor sit amet 2<br>");
+      }
+
+      basic();
+      basic();
+      basic();
+     ?>
+  </body>
+</html>
