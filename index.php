e37f3f084e52510c51d892e2571c7a01e84f09c8
Adella-Jo <70190535+Adella-Jo@users.noreply.github.com>
2020-08-31 18:19:36 +0900

    Web1
    함수를 활용해서 중복되는 코드를 간단하게 정리할 수 있다. ; 보기도 더 간편하더랑

--- index.php
+++ index.php
@@ -1,25 +1,51 @@
+<?php
+function print_title(){
+  if(isset($_GET['id'])) {
+    echo $_GET['id'];
+  } else {
+    echo "Welcome";
+  }
+}
+function print_description(){
+  if(isset($_GET['id'])) {
+    echo file_get_contents("data/".$_GET['id']);
+  } else {
+    echo "Hello, PHP";
+  }
+}
+function print_list(){
+  $list = scandir('./data');
+  $i = 0;
+  while($i < count($list)){
+    if($list[$i] != '.') {
+      if($list[$i] != '..') {
+
+    echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n"; } }
+    $i = $i + 1;
+
+  }
+}
+ ?>
+
 <!doctype html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>
+      <?php
+        print_title();
+
+       ?>
+
+
+    </title>
   </head>
   <body>
     <h1><a href="index.php">WEB</a></h1>
     <ol>
       <?php
-        $list = scandir('./data');
 
-        $i = 0;
-        while($i < count($list)){
-          if($list[$i] != '.') {
-            if($list[$i] != '..') {
-
-          echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n"; } }
-          $i = $i + 1;
-
-        }
-
+      print_list();
 
         /*
         echo "<li>$list[0]</li>\n";
@@ -34,20 +60,12 @@
     </ol>
     <h2>
       <?php
-        if(isset($_GET['id'])) {
-          echo $_GET['id'];
-        } else {
-          echo "Welcome";
-        }
+        print_title();
       ?>
     </h2>
 
     <?php
-      if(isset($_GET['id'])) {
-        echo file_get_contents("data/".$_GET['id']);
-      } else {
-        echo "Hello, PHP";
-      }
+      print_description();
      ?>
 
 
