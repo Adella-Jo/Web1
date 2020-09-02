c984c02630493bd71d33de557b611349771f2402
Adella-Jo <70190535+Adella-Jo@users.noreply.github.com>
2020-09-03 00:03:11 +0900

    Web1
    목차에 추가가 되면서 흰 화면이 나오는 것이 아니라 다시 redirection 하는 것

--- create_process.php
+++ create_process.php
@@ -1,4 +1,6 @@
 <?php
-file_put_contents('data/'.$_POST['title'], $_POST['description']); 
+file_put_contents('data/'.$_POST['title'], $_POST['description']);
+/* 이거 하다가 머리 터지면 어떻게 하지ㅎㅎ */
 
+header('Location: /index.php?id='.$_POST['title']);
  ?>
