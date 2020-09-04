87963c004380f72ee7f242090cf61974bad46311
Adella-Jo <70190535+Adella-Jo@users.noreply.github.com>
2020-09-04 21:02:16 +0900

    Web1
    오늘은 php를 이용해서 update 란을 만들고, 그걸 이용하여 글 제목과 내용을 수정할 수 있게 함. 게다가 자동으로 data 폴더에의 제목도 자동수정이 가능하게 함.

--- update.php
+++ update.php
@@ -75,6 +75,18 @@ function print_list(){
       print_description();
      ?>
 
+     <form action="update_process.php" method="post">
+       <input type="hidden" name="old_title" value="<?=$_GET['id']?>">
+       <p>
+         <input type="text" name="title" placeholder="title" value="<?php print_title(); ?>">
+       </p>
+       <p>
+         <textarea name="description" placeholder="description"><?php print_description(); ?></textarea>
+       </p>
+       <p>
+         <input type="submit">
+       </p>
+     </form>
 
   </body>
 </html>
--- update_process.php
+++ update_process.php
@@ -1,7 +1,6 @@
 <?php
+rename('data/'.$_POST['old_title'], 'data/'.$_POST['title']);
 file_put_contents('data/'.$_POST['title'], $_POST['description']);
-/* 이거 하다가 머리 터지면 어떻게 하지ㅎㅎ */
-
 header('Location: /index.php?id='.$_POST['title']);
 
  ?>
