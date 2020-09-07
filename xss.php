67db4e623f92b4042898c93c9484a69653c14e9e
Adella-Jo <70190535+Adella-Jo@users.noreply.github.com>
2020-09-07 22:01:58 +0900

    Web1
    보안

---
+++ xss.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <head>
+    <mata charset="utf-8">
+    <title>XSS</title>
+  </head>
+<body>
+  <h1>Cross site scripting</h1>
+  <?php
+    echo htmlspecialchars('<script>alert("babo");</script>');
+  ?>
+</body>
+</html>
